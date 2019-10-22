d46df121f2200d30eedeed89913fc5b063c198bd
Oleksii Kolomiiets <oleksii@snappmobile.io>
2019-10-22 19:40:02 +0300

    Initial Commit

---
+++ TestOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestOne
+//
+//  Created by  Oleksii Kolomiiets on 10/22/19.
+//  Copyright © 2019  Oleksii Kolomiiets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
